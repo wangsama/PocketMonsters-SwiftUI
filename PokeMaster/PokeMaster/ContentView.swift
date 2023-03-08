4125980456de2eba9c7150d5695559f33ccb7a10
madao <madao@ponyft.com>
2023-03-08 15:37:00 +0800

    添加一些资源和 utils

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by madao on 2023/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeMaster/PokeMaster/PokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterApp.swift
+//  PokeMaster
+//
+//  Created by madao on 2023/3/7.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
